b00a21dae3957b825f395725e8e3fa24f9b49c88
papapapapapapapa <r3386588@gmail.com>
2017-10-15 01:39:38 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner in=new Scanner(System.in);
+        float x;
+        x=in.nextFloat();
+        System.out.printf("%.2f",x*9/5+32);
+        System.out.println();
+    }
+}
